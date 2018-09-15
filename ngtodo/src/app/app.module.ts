6c665ae1e456a6c002270d7f08d1b5876ffc0268
shivanagoudagooli <shivanagoudagooli@gmail.com>
2018-09-15 14:00:29 +0530

    added createtodo component

--- ngtodo/src/app/app.module.ts
+++ ngtodo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatetodoComponent } from './createtodo/createtodo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatetodoComponent
   ],
   imports: [
     BrowserModule,
---
+++ ngtodo/src/app/createtodo/createtodo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-createtodo',
+  templateUrl: './createtodo.component.html',
+  styleUrls: ['./createtodo.component.css']
+})
+export class CreatetodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
